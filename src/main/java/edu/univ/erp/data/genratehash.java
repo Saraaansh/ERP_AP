35cfaecb3ebb92490f06732d10fab306a34dc286
Saransh Gupta <saransh24508@iiitd.ac.in>
2026-01-13 23:07:08 +0530

    Delete src directory

--- src/main/java/edu/univ/erp/data/genratehash.java
+++
@@ -1,9 +0,0 @@
-// Utility to generate BCrypt password hashes for testing
-package edu.univ.erp.data;
-import edu.univ.erp.auth.PasswordHasher;
-public class genratehash {
-    public static void main(String[] args) {
-        String hash = PasswordHasher.hash("password123");
-        System.out.println("New Hash: " + hash);
-    }
-}
